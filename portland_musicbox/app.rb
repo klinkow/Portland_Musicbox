f7b7281f09a2ee5ca3956f6124bc8567c2286dd9
Devin Ludwig <devinludwig@gmail.com>
2016-12-20 15:16:58 -0800

    fix label_id for new labels.

--- portland_musicbox/app.rb
+++ portland_musicbox/app.rb
@@ -89,10 +89,11 @@ post "/album/new" do
   @user = User.find_by({:current => true})
   @artist = Artist.find_by(user_id: @user.id)
   if params['new_label'] != ''
-    label_id = Label.create(name: params['new_label'], est_date: params['new_date'])
+    label_id = Label.create(name: params['new_label'], est_date: params['new_date']).id
   else
     label_id = Label.find(params['label']).id
   end
+  binding.pry
   @artist.albums.push(Album.create(name: params['name'], credits: params["credits"], album_photo_name: params["album_art"], label_id: label_id, music_embed: params["embed"]))
   @album = Album.find_by(:name => params["name"])
   tracks = params["tracks"]
