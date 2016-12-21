1021eb51b3e4bb9ed6902f96bd6dfded2fcbb624
Devin Ludwig <devinludwig@gmail.com>
2016-12-21 11:05:43 -0800

    remove 'require from some new account form tags; add content to labels.erb; add to seeds.rb including track creation loop; add branch to user_dash; add 'label has_many albums'

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 require '/Users/Guest/Desktop/portland_musicbox/lib/label.rb'
 require("/Users/Guest/Desktop/portland_musicbox/lib/artist.rb")
 require("/Users/Guest/Desktop/portland_musicbox/lib/album.rb")
+require("/Users/Guest/Desktop/portland_musicbox/lib/track.rb")
 
-
-labels = ['Kill Rock Stars', 'Tender Loving Empire', 'Good Cheer', 'Golden Brown', 'Virgin', 'Dischord', 'GOOD Music', 'Geographic North', 'Straight']
-founded = [1991, 2005, 2014, 2016, 1972, 2004, 2007, 1969]
+labels = ['Kill Rock Stars', 'Tender Loving Empire', 'Good Cheer', 'Golden Brown', 'Virgin', 'Dischord', 'GOOD Music', 'Geographic North', 'Straight', 'Glacial Pace']
+founded = [1991, 2005, 2014, 2016, 1972, 1980, 2004, 2007, 1969, 2005]
 labels.each do |label|
    Label.create(name: label, est_date: founded[labels.index(label)])
 end
@@ -28,4 +28,13 @@ Album.create(name: "In Summer", credits: "Jefre Cantu-Ledesma has described his 
 
 @artist5 = Artist.create(name: "Captain Beefheart")
 artist5_id = @artist5.id
-Album.create(name: "Trout Mask Replica", credits: "Trout Mask Replica is the third album by Captain Beefheart and his Magic Band, released in June 1969. Produced by Beefheart's friend and former schoolmate Frank Zappa, it was originally released as a double album on Zappa's Straight Records label. Combining elements of R&B, garage rock, blues, avant-garde, free jazz and other genres of American music,[1] the album is regarded as an important work of experimental music and art rock.[2]", artist_id: artist5_id, label_id: Label.find_by(name: "Straight").id, album_photo_name: "http://img2-ak.lst.fm/i/u/ar0/88690231fafe417fb22aebc819005158")
+Album.create(profile_photo: "http://img.wennermedia.com/social/rs-143557-rectangle.jpg" name: "Trout Mask Replica", credits: "Trout Mask Replica is the third album by Captain Beefheart and his Magic Band, released in June 1969. Produced by Beefheart's friend and former schoolmate Frank Zappa, it was originally released as a double album on Zappa's Straight Records label. Combining elements of R&B, garage rock, blues, avant-garde, free jazz and other genres of American music,[1] the album is regarded as an important work of experimental music and art rock.[2]", artist_id: artist5_id, label_id: Label.find_by(name: "Straight").id, album_photo_name: "http://img2-ak.lst.fm/i/u/ar0/88690231fafe417fb22aebc819005158")
+
+@artist6 = Artist.create(name: "Mimicking Birds")
+artist6_id = @artist6.id
+birds = Album.create(name: "Eons", credits: "On his debut as Mimicking Birds, Portland’s Nate Lacy exposed a rare gift for cyclical melody and atmospheric soul-searching. On Eons, his sophomore effort for Glacial Pace Recordings, Lacy opens up his internal world into a vast starry landscape where “Mountains slide away from their fault lines / Taper into new shorelines / Sliding down the timeline / Pull the nerves up through your spine.", artist_id: artist6_id, label_id: Label.find_by(name: "Glacial Pace").id, album_photo_name: "http://static.stereogum.com/uploads/2014/05/Mimicking-Birds-Eons-608x608.jpg")
+tracks = ["Memorabilia", "Acting Your Age", "Owl Hoots", "Spent Winter", "Bloodlines", "Night Light", "Water Under Burned Bridges", "Wormholes", "Seeing Eye Dog", "Moving On"]
+length = ["3:22", "3:40", "5:11", "4:41", "3:57", '5:17', '5:40','3:14','3:38','5:06']
+tracks.each do |track|
+  Track.create(album_id: birds.id, artist_id: artist6_id, name: track, track_length: length[tracks.index(track)], track_number: (tracks.index(track)+1))
+end
--- spec/label_spec.rb
+++ spec/label_spec.rb
@@ -2,4 +2,5 @@ require "spec_helper"
 
 describe Label do
   it {should have_and_belong_to_many :artists}
+  it {should have_many :albums}
 end
